ee97d705c140edb49e24ef99d6d5ad981a414936
frankhart2018 <sdhargithub@gmail.com>
2025-07-25 18:44:46 -0500

    Navigate to root when nav title is clicked

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import GodModeNav from "./GodModeNav";
 import ThemeToggle from "./ThemeToggle";
-import { useTheme } from "../contexts/ThemeContext";
+import { useTheme } from "../contexts/useTheme";
+import { useNavigate } from "react-router-dom";
 import "./Navbar.css";
 
 interface NavbarProps {
@@ -10,12 +11,14 @@ interface NavbarProps {
 
 const Navbar: React.FC<NavbarProps> = ({ pageTitle }) => {
   const { theme, toggleTheme } = useTheme();
+  const navigate = useNavigate();
+
   return (
     <div className="chatbot-header">
       <div className="navbar-left">
         <GodModeNav />
       </div>
-      <div className="chatbot-header-title">ResQTalk - {pageTitle}</div>
+      <div className="chatbot-header-title" onClick={() => navigate('/')}>ResQTalk - {pageTitle}</div>
       <div className="navbar-right">
         <ThemeToggle theme={theme} toggleTheme={toggleTheme} />
       </div>
